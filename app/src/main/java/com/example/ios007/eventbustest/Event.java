73aa8c4471e9af42ecdcba5d6e4018b1e83adbd6
mahui <760473233@qq.com>
2017-07-12 13:49:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ios007/eventbustest/Event.java
@@ -0,0 +1,18 @@
+package com.example.ios007.eventbustest;
+
+import java.util.List;
+
+public class Event {
+	/** 列表加载事件 */
+	public static class ItemListEvent {
+		private List<Item> items;
+
+		public ItemListEvent(List<Item> items) {
+			this.items = items;
+		}
+
+		public List<Item> getItems() {
+			return items;
+		}
+	}
+}
